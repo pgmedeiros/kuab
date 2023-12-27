dbc93e1158a7dda1aa8cd74d8ab1dc6fd768e5fe
Pedro Medeiros <emaildopedromedeiros@gmail.com>
2023-12-27 01:34:56 -0300

    adiciona bloco de video

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,11 +1,25 @@
-import Video from '@/app/course/class/page'
+import styles from '@/app/app.module.css'
 
 export default function Course() {
 
     return(
-        <ol>
-            <li> <Video/></li>
-            <li> <Video/></li>
-        </ol>
+        <div className= {styles.main}>
+            <Video/>
+            <Video/>
+        </div>
     );
 }
+
+function Video() {
+    return(
+    <div className={ styles.videoBlock }>
+        <iframe 
+        className={ styles.videoIframe } 
+        src="https://www.youtube.com/embed/h_cscbKu-lY?si=jg_0vx1FqFh-v76b" 
+        title="YouTube video player" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
+      </iframe>
+      <p>Acesse o original: LINK PARA O YOUTUBE</p>
+    </div>           
+    );
+  }

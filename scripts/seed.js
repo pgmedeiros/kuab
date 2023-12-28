f02bb632d2e1ec65d4c3ba6b10bb8ac125b21fc7
Pedro Medeiros <emaildopedromedeiros@gmail.com>
2023-12-28 17:31:01 -0300

    altera seed para lectures

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,335 +1,64 @@
 const { db } = require('@vercel/postgres');
-const bcrypt = require('bcrypt');
 
-const users = [
+const lectures = [
     {
-      id: '410544b2-4001-4271-9855-fec4b6a6442a',
-      name: 'User',
-      email: 'user@nextmail.com',
-      password: '123456',
+      id: 'd7eaa454-ccec-4404-bde4-6c69603b7cb9',
+      link: 'https://www.youtube.com/watch?v=h_cscbKu-lY&list=PL5TPkym335qztD0JofGEyVcdmlNf7arzE',
+      position: '1',
+      course: 'AED1',
+      university: 'ufscar'
     },
+    {
+      id: 'f0823cbf-0901-427e-830f-48a3ca858d4a',
+      link: 'https://www.youtube.com/watch?v=1Uz8wXKdzcA&list=PL5TPkym335qztD0JofGEyVcdmlNf7arzE&index=2',
+      position: '2',
+      course: 'AED1',
+      university: 'ufscar'
+    },
+    {
+      id: 'ce617aff-befc-4e86-871a-ff74ea78699e',
+      link: 'https://www.youtube.com/watch?v=X60pMaSdo2A&list=PL5TPkym335qztD0JofGEyVcdmlNf7arzE&index=3',
+      position: '3',
+      course: 'AED1',
+      university: 'ufscar'
+    }
   ];
-  
-const customers = [
-    {
-        id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-        name: 'Delba de Oliveira',
-        email: 'delba@oliveira.com',
-        image_url: '/customers/delba-de-oliveira.png',
-    },
-    {
-        id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-        name: 'Lee Robinson',
-        email: 'lee@robinson.com',
-        image_url: '/customers/lee-robinson.png',
-    },
-    {
-        id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
-        name: 'Hector Simpson',
-        email: 'hector@simpson.com',
-        image_url: '/customers/hector-simpson.png',
-    },
-    {
-        id: '50ca3e18-62cd-11ee-8c99-0242ac120002',
-        name: 'Steven Tey',
-        email: 'steven@tey.com',
-        image_url: '/customers/steven-tey.png',
-    },
-    {
-        id: '3958dc9e-787f-4377-85e9-fec4b6a6442a',
-        name: 'Steph Dietz',
-        email: 'steph@dietz.com',
-        image_url: '/customers/steph-dietz.png',
-    },
-    {
-        id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
-        name: 'Michael Novotny',
-        email: 'michael@novotny.com',
-        image_url: '/customers/michael-novotny.png',
-    },
-    {
-        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-        name: 'Evil Rabbit',
-        email: 'evil@rabbit.com',
-        image_url: '/customers/evil-rabbit.png',
-    },
-    {
-        id: '126eed9c-c90c-4ef6-a4a8-fcf7408d3c66',
-        name: 'Emil Kowalski',
-        email: 'emil@kowalski.com',
-        image_url: '/customers/emil-kowalski.png',
-    },
-    {
-        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
-        name: 'Amy Burns',
-        email: 'amy@burns.com',
-        image_url: '/customers/amy-burns.png',
-    },
-    {
-        id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
-        name: 'Balazs Orban',
-        email: 'balazs@orban.com',
-        image_url: '/customers/balazs-orban.png',
-    },
-];
 
-const invoices = [
-    {
-        customer_id: customers[0].id,
-        amount: 15795,
-        status: 'pending',
-        date: '2022-12-06',
-    },
-    {
-        customer_id: customers[1].id,
-        amount: 20348,
-        status: 'pending',
-        date: '2022-11-14',
-    },
-    {
-        customer_id: customers[4].id,
-        amount: 3040,
-        status: 'paid',
-        date: '2022-10-29',
-    },
-    {
-        customer_id: customers[3].id,
-        amount: 44800,
-        status: 'paid',
-        date: '2023-09-10',
-    },
-    {
-        customer_id: customers[5].id,
-        amount: 34577,
-        status: 'pending',
-        date: '2023-08-05',
-    },
-    {
-        customer_id: customers[7].id,
-        amount: 54246,
-        status: 'pending',
-        date: '2023-07-16',
-    },
-    {
-        customer_id: customers[6].id,
-        amount: 666,
-        status: 'pending',
-        date: '2023-06-27',
-    },
-    {
-        customer_id: customers[3].id,
-        amount: 32545,
-        status: 'paid',
-        date: '2023-06-09',
-    },
-    {
-        customer_id: customers[4].id,
-        amount: 1250,
-        status: 'paid',
-        date: '2023-06-17',
-    },
-    {
-        customer_id: customers[5].id,
-        amount: 8546,
-        status: 'paid',
-        date: '2023-06-07',
-    },
-    {
-        customer_id: customers[1].id,
-        amount: 500,
-        status: 'paid',
-        date: '2023-08-19',
-    },
-    {
-        customer_id: customers[5].id,
-        amount: 8945,
-        status: 'paid',
-        date: '2023-06-03',
-    },
-    {
-        customer_id: customers[2].id,
-        amount: 8945,
-        status: 'paid',
-        date: '2023-06-18',
-    },
-    {
-        customer_id: customers[0].id,
-        amount: 8945,
-        status: 'paid',
-        date: '2023-10-04',
-    },
-    {
-        customer_id: customers[2].id,
-        amount: 1000,
-        status: 'paid',
-        date: '2022-06-05',
-    },
-];
-
-const revenue = [
-    { month: 'Jan', revenue: 2000 },
-    { month: 'Feb', revenue: 1800 },
-    { month: 'Mar', revenue: 2200 },
-    { month: 'Apr', revenue: 2500 },
-    { month: 'May', revenue: 2300 },
-    { month: 'Jun', revenue: 3200 },
-    { month: 'Jul', revenue: 3500 },
-    { month: 'Aug', revenue: 3700 },
-    { month: 'Sep', revenue: 2500 },
-    { month: 'Oct', revenue: 2800 },
-    { month: 'Nov', revenue: 3000 },
-    { month: 'Dec', revenue: 4800 },
-];
-  
-async function seedUsers(client) {
+async function seedClassAlgorithmOne(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Create the "users" table if it doesn't exist
+    // Create the "lectures" table if it doesn't exist
     const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS users (
+      CREATE TABLE IF NOT EXISTS lectures (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
+        link VARCHAR(1000) NOT NULL,
+        position TEXT NOT NULL,
+        course TEXT NOT NULL,
+        university TEXT NOT NULL
       );
     `;
 
-    console.log(`Created "users" table`);
+    console.log(`Created "lectures" table`);
 
-    // Insert data into the "users" table
-    const insertedUsers = await Promise.all(
-      users.map(async (user) => {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
+    // Insert data into the "lectures" table
+    const insertedLectures = await Promise.all(
+      lectures.map(async (lecture) => {
         return client.sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        INSERT INTO lectures (id, link, position, course, university)
+        VALUES (${lecture.id}, ${lecture.link}, ${lecture.position}, ${lecture.course}, ${lecture.university})
         ON CONFLICT (id) DO NOTHING;
       `;
       }),
     );
 
-    console.log(`Seeded ${insertedUsers.length} users`);
+    console.log(`Seeded ${insertedLectures.length} lectures`);
 
     return {
       createTable,
-      users: insertedUsers,
+      lectures: insertedLectures,
     };
   } catch (error) {
-    console.error('Error seeding users:', error);
-    throw error;
-  }
-}
-
-async function seedInvoices(client) {
-  try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-    // Create the "invoices" table if it doesn't exist
-    const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS invoices (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    customer_id UUID NOT NULL,
-    amount INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    date DATE NOT NULL
-  );
-`;
-
-    console.log(`Created "invoices" table`);
-
-    // Insert data into the "invoices" table
-    const insertedInvoices = await Promise.all(
-      invoices.map(
-        (invoice) => client.sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedInvoices.length} invoices`);
-
-    return {
-      createTable,
-      invoices: insertedInvoices,
-    };
-  } catch (error) {
-    console.error('Error seeding invoices:', error);
-    throw error;
-  }
-}
-
-async function seedCustomers(client) {
-  try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-    // Create the "customers" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS customers (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        image_url VARCHAR(255) NOT NULL
-      );
-    `;
-
-    console.log(`Created "customers" table`);
-
-    // Insert data into the "customers" table
-    const insertedCustomers = await Promise.all(
-      customers.map(
-        (customer) => client.sql`
-        INSERT INTO customers (id, name, email, image_url)
-        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedCustomers.length} customers`);
-
-    return {
-      createTable,
-      customers: insertedCustomers,
-    };
-  } catch (error) {
-    console.error('Error seeding customers:', error);
-    throw error;
-  }
-}
-
-async function seedRevenue(client) {
-  try {
-    // Create the "revenue" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS revenue (
-        month VARCHAR(4) NOT NULL UNIQUE,
-        revenue INT NOT NULL
-      );
-    `;
-
-    console.log(`Created "revenue" table`);
-
-    // Insert data into the "revenue" table
-    const insertedRevenue = await Promise.all(
-      revenue.map(
-        (rev) => client.sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
-      `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedRevenue.length} revenue`);
-
-    return {
-      createTable,
-      revenue: insertedRevenue,
-    };
-  } catch (error) {
-    console.error('Error seeding revenue:', error);
+    console.error('Error seeding lectures:', error);
     throw error;
   }
 }
@@ -337,11 +66,7 @@ async function seedRevenue(client) {
 async function main() {
   const client = await db.connect();
 
-  await seedUsers(client);
-  await seedCustomers(client);
-  await seedInvoices(client);
-  await seedRevenue(client);
-
+  await seedClassAlgorithmOne(client);
   await client.end();
 }
 

de4f8519bd7f562a36d5e362e54692613a4e7479
Pedro Medeiros <emaildopedromedeiros@gmail.com>
2023-12-31 09:38:39 -0300

    adiciona sql de aulas

---
+++ scripts/seed_data.sql
@@ -0,0 +1,88 @@
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Cálculo I', 'Familiarizar o aluno com as noções de limite, derivada e integral de funções de uma variável, destacando aspectos geométricos e interpretações físicas.', '1');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=eGFYmV-IAudcJJ2j&amp;list=PLAudUnJeNg4tr-aiNyYCXE46L3qEZ2Nzx', '3f9d4194-cc71-41bc-8043-bf366ab8ad39');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Matemática Discreta', 'Introdução a lógica matemática e estratégias de provas; conjuntos; relações e ordens; indução matemática; princípios de contagem; relações de recorrência; grafos.', '1');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=gPN0luNhWsvv_wT4&amp;list=PL6mfjjCaO1WrEJ0JKRyXO3QjaPkJaSvAS', 'fa62d06f-8cee-4a4e-8eb5-866e1681cb48');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Circuitos Digitais', 'Códigos, Números e Aritmética. Álgebra Booleana. Portas Lógicas. Circuitos integrados. Laboratório de Circuitos Hardware. Simulador de Circuitos Digitais. Laboratório de Circuitos Software. Circuitos Combinatórios. Circuitos Seqüenciais. Circuitos básicos dos componentes do Computador Digital. Projeto e construção de CPU de 4bits. Projeto de linguagem de máquina e de unidade de controle micro programada. Circuitos Aritméticos e Unidade Lógica Aritmética. Memória de Controle dos micro programas e Unidade de Controle. Memória Interna de Registradores do processador. Memória Principal ROM e RAM.', '1');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=wbRZJIGIdACTt_yo&amp;list=PLXyWBo_coJnMYO9Na3t-oYsc2X4kPJBWf', 'e26d1c1c-fef3-4ff8-8655-4c2de1476d2d');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Introdução à Ciência da Computação', 'Introdução à Lógica. Tipos de dados e instruções Primitivas. Estruturas de controle para a tomada de decisões. Estruturas de controle de repetição. Estruturas de dados homogêneas (vetores e matrizes). Utilização de funções e parâmetros. Recursividade. Estruturas de dados heterogêneas (registros). Noções básicas de arquivos. Noções básicas de alocação dinâmica de memória e uso de ponteiros.', '1');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=8mqukY7KmnFSyfxB&amp;list=PL892UsUS6uKUR9iF7bSju72VRhQvJhtWH', 'a73d162c-2bc5-4402-a463-14cbda1a4a39');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Cálculo II', 'Funções de duas ou mais variáveis: limites, continuidade, diferenciabilidade -- Gradiente -- Regra da cadeia -- Teorema do Valor Médio -- Derivadas de ordem superior -- Teorema de Schwarz (enunciado) -- Fórmula de Taylor -- Máximos e Mínimos -- Multiplicadores de Lagrange.', '2');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=49TO2rbX_BiC6D0_&amp;list=PLAudUnJeNg4sd0TEJ9EG6hr-3d3jqrddN', 'ff42c94c-9984-4a0f-b7ff-48107de84898');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Álgebra Linear', 'Matrizes. Sistemas lineares. Vetores. Bases. Autovalores e autovetores. Diagonalização de Matrizes.', '2');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=38eisuiWooahZX-9&amp;list=PLIEzh1OveCVczEZAjhVIVd7Qs-X8ILgnI', '20bae3cf-9ac7-4ca0-8cd1-71078382887d');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Transmissão de Dados', 'Conceitos básicos. Transmissão da Informação. Topologias e meios físicos de transmissão. Arquiteturas e padrões. Modelo OSI da ISO. Modelo TCP/IP. Camada Física. Camada de Enlace.', '2');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=waiqNyAKA4XxQwf1&amp;list=PLeGZZBT0PPMcXkyTtsSmYNBbh_zFb8R4T', '69c29726-0d93-4a46-87b5-6512e3d98338');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=zAJDeYojeePNGlTw&amp;list=PLeGZZBT0PPMcB2jERaYqfkzmXWUe-M4Wx', '69c29726-0d93-4a46-87b5-6512e3d98338');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=Kyr_Jr3vbUmY3dkh&amp;list=PLeGZZBT0PPMfErOHzA8raqFIk-3QY02eu', '69c29726-0d93-4a46-87b5-6512e3d98338');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Lógica para Computação', 'Introdução: Origem, Revisão Matemática (Indução e Recursão); Lógica Proposicional e de Predicados: Linguagem, Semântica, Sistemas Dedutivos; Teoremas Principais: Corretude, Completude, Substituição, Forma Normal de Fórmulas; Decidibilidade e Complexidade; Aplicações à Computação.', '3');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=yh1_baZghpI3yYaY&amp;list=PLYLYA7XrlskOyvh474MedmZ9-hNkeSYoL', '8d478af1-d4cc-4417-8fd1-91437b61cd35');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Introdução a Probabilidade e Estatística', 'Probabilidade; Variáveis Aleatórias; Distribuições de Probabilidade: Binomial, Hipergeométrica, Poisson e Normal. Noções sobre Amostragem e Distribuições Amostrais. Estimação. Testes de Hipóteses. Regressão e Correlação.', '3');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=fSzc5mR7bu_3iFxP&amp;list=PL-20Z1XFWKR1JGnotqkqH98I2xMrVqpv-', 'e476b9e2-429b-4664-9bec-b46136ebb93a');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Algoritmos e Estrutura de Dados II', 'Desenvolvimento e análise de algoritmos gulosos e baseados em programação dinâmica. Estruturas de dicionário em memória secundária. Tabelas Hash e suas aplicações. Tries e suas aplicações. Implementações e algoritmos de grafos.', '3');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=3EmD1fGJIHJUz2o5&amp;list=PL5TPkym335qzwUkBSiM0uIphAPjHFDNbB', '5364bcb9-0473-45b1-a7cd-87d2db4381f9');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Arquitetura de Computadores', 'Evolução e desempenho do computador, funções de um computador, memórias, dispositivos de entrada e saída, estrutura e função de um processador, conjunto de instruções : características, funções e modos de endereçamento.', '3');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=SV-oq4-Ab2nTLoLt&amp;list=PLR2tpXhN7CHd9MTiglTMUCyeAoEtjXzRw', '4bbc49e3-8c98-4719-9e48-e79c68b3c0cf');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Técnicas de Programação I', 'Conceitos básicos de orientação a objetos (OO): classe, objeto, atributo, método, instanciação, encapsulamento, herança, polimorfismo, sobrecarga, sobrescrita, ligação precoce (early binding), ligação tardia (late binding) e interfaces. Prática de programação em uma linguagem OO. Princípios de projeto OO: acoplamento, coesão e S.O.L.I.D. Entrada/saída. Tratamento de exceção.', '3');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=kyjoHaPQyEg85u3e&amp;list=PL3JRjVnXiTBZG4cY0z5PG7J0EUyRQCkSa', 'cd21afe2-ad6e-4386-ad90-926da18a4503');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Projeto e Análise de Algoritmos', 'Análise de algoritmos. Algoritmos de Ordenação e Divisão e Conquista. Programação Dinâmica. Algoritmos Gulosos. Complexidade Computacional.', '4');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=Ezc2Bl1bqG0rqyTF&amp;list=PL5TPkym335qxZktT1RZD1RGN3P6Iey1fp', '48850f14-0155-4cdd-b576-64b181b3023b');
+
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Métodos Numéricos I', 'Introdução a Métodos Numéricos; Teoria dos Erros; Raízes de Equações Algébricas Não- Lineares; Sistemas de Equações Algébricas Lineares; Aproximação Numérica.', '4');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=gmSCYhgUB1zNlsi5&amp;list=PLpVCamz0zLr-4qAqWQRf8B5U7pcIb1cQc', '9de56054-71ea-407f-b3a0-c1742b453476');
+
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Computação Gráfica', 'Sistemas Gráficos e Modelos; Programação Gráfica; Input e Interação; Objetos Geométricos e Transformações; Visualização; Pintura; Modelos de Iluminação Local.', '4');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=zRw_tt3kxdTOI7kE&amp;list=PLX6Nyaq0ebfgTrw6BlgBpPwM3F7I4UMyA', '941b363c-cb3c-44f6-8dde-4263b536a66a');
+
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Métodos Numéricos II', 'Integração Numérica; Diferenciação Numérica; Cálculo de Autovalores e Autovetores de Matrizes; Solução de Problemas de Valores Iniciais de Equações Diferenciais Ordinárias; Solução de Problemas de Valores de Contorno de Equações Diferenciais.', '5');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=Ohsn-J-ByQwbZwAP&amp;list=PLpVCamz0zLr-xYIt0oJayXHVpI7_S2Ai7', '9ccfe6ac-1aab-4401-8807-f63eb43d284c');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Banco de Dados', 'Arquiteturas de sistemas de gerenciamento de bancos de dados. Modelagem de dados: modelos conceituais e lógicos, incluindo o modelo relacional e normalização. Algebra relacional. Linguagens. Otimização de consultas. Mecanismos de proteção, recuperação e segurança. Controle de concorrência. Bancos de dados não relacionais. Projeto e desenvolvimento de ferramentas e técnicas utilizadas na solução de problemas de sistemas de informação, utilizando bancos de dados. Modelagem, especificação, projeto e implementação de aplicações em sistemas de informação.', '4');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=18XGNdddJjWDvPKT&amp;list=PL3JRjVnXiTBZ2EMkni7hyqdr-JChtVIwB', 'd774f492-f948-44a8-8852-c4781376522c');
+
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Engenharia de Software I', '1. Gerenciamento de projeto; 2. Estimação de custos; 3. Análise e especificação de requisitos; 4. Especificações formais; 5. Interface com o usuário; 6. Modelagem de dados; 7. Técnicas e modelagens para projeto e implementação: arquitetura de projeto, projeto estruturado, projeto orientado a objetos; 8. Gerenciamento de versões e configurações; 9. Verificação: testes, revisões e inspeções; 10. Validação e certificação de qualidade; 11. Manutenção; 12. Documentação.', '5');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=j2AvgeoDjaMXmvfd&amp;list=PLhBaeEzs3d7lsn_Mq2n3R4_api16Wkp1Q', 'fd32d9d5-5c21-4a34-aa84-d905d47b1795');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Redes de Computadores I', 'Introdução. Topologias de redes e a Internet. Arquiteturas e padrões. Camadas do Modelo de Referência da Internet (TCP/IP) - Aplicação, Transporte, Rede e Enlace.', '5');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=H5znMxNOB4L-fRVQ&amp;list=PLvHXLbw-JSPfmuqtNUhQT16b1He0MbTJi', 'b931348f-2f44-4783-bee7-6fa02468c3f6');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Linguagens de Programação', 'Nomes e Vinculações. Escopo estático e dinâmico. Tipos de dados. Expressões e sentenças de atribuição. Subprogramas.', '4');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=efkZ7iMM5Q3IgAID&amp;list=PLnzT8EWpmbka4KukGR184tifzqcuq_ZDv', 'a8acf668-1254-4bda-8ef5-decc47793397');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Paradigmas de Programação', 'O modelo de computação declarativo; o modelo de computação declarativo concorrente; o modelo de computação com estado; o modelo de computação orientado a objetos.', '5');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=aGRZYJhIfMF1BBdm&amp;list=PLsri1g4fxrjuf6UCYHqCmqsfXR4gofAFH', 'e850bbc6-2be3-4ae8-a406-554fe6b6e175');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Inteligência Artificial', 'Estratégias de busca com e sem informação, busca para jogos, meta-heurísticas para otimização, modelos gráficos probabilísticos e aprendizado de máquina.', '6');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=OyoaQUbEdZ4oBGvB&amp;list=PLeejGOroKw_txh7j7S3etF5eudI2WvMx0', '73dd02a0-c244-4bbe-a7ad-041f376362ff');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Sistemas Operacionais', 'Conceitos de sistemas operacionais; Gerência de processos; Gerência de memória; Sistemas de arquivo; Gerência de dispositivos de entrada/saída; Problemática dos deadlocks.', '6');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=XN8ptYhgQSRPh_-j&amp;list=PL6i520yqwpsT-Zjj6jf9PA8M2oPk80ypf', '6d9bc9df-2b34-4d67-98fb-300b6a65d408');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Análise e Projeto de Sistemas I', 'Fundamentos de Sistemas: Conceitos e Componentes. Sistemas de Informação por Computador. Modelagem de Sistemas. Técnicas de Modelagem: Estruturada e Orientada a Objetos. Linguagem de Modelagem Unificada UML. Mapeamento OO para Entidade Relacionamento.', '6');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=H0IC9X47khw8TF1f&amp;list=PLnzT8EWpmbkbTSVytDfmxS7hjIL1lDA3O', 'c52096ca-be92-431d-ad9e-a7795473ee7f');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Autômatos e Linguagens Formais', 'Linguagens: Regulares, Livres de Contexto, Sensíveis ao Contexto, Irrestritas; Operações com linguagens; Propriedades das Linguagens; Lema do Bombeamento para linguagens regulares e para linguagens livres de contexto; Geradores de Linguagens: Expressões Regulares, Gramáticas Livres de Contexto; Reconhecedores: Autômatos Finitos Determinísticos, Autômatos Finitos Não Determinísticos, Autômatos de Pilha.', '6');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=hpxd0gWUEK6Yu4EV&amp;list=PLncEdvQ20-mhD_qMeLHtLnA3XDT1Fr_k4', '47584cc4-c267-442d-aa6d-8d2502151d71');
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Compiladores', 'Introdução à Compilação; Análise Léxica; Análise Sintática; Análise Semântica; Geração de Código; Otimização de Código.', '7');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=rR7goRfTD1jtyZad&amp;list=PLX6Nyaq0ebfhI396WlWN6WlBm-tp7vDtV', '7729882f-eded-4267-8459-f96ed4e427a2');
+
+
+insert into courses (id, name, description, semester) values (uuid_generate_v4(), 'Teoria da Computação', 'Introdução à Compilação; Análise Léxica; Análise Sintática; Análise Semântica; Geração de Código; Otimização de Código.', '7');
+insert into lectures(id, link, id_course) values (uuid_generate_v4(), 'https://www.youtube.com/embed/videoseries?si=Fo0Gcy5D5COO8nqu&amp;list=PLeejGOroKw_tKmxs0zB7XzUR4Pb8omSH9', '89c4549d-5f91-47e4-a8d0-ef233589628e');
+

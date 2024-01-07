bb05fe7d5e009eed370e19464c5acebbd109a3cf
Pedro Medeiros <emaildopedromedeiros@gmail.com>
2024-01-06 21:44:33 -0300

    adiciona height para elementos navbar

--- app/lib/navbar.tsx
+++ app/lib/navbar.tsx
@@ -7,14 +7,14 @@ export default function Navbar() {
     return(
         <nav className={styles.navbar}>
             <div className={styles.internalNav}>
-                <Link className={styles.title} href="/">Home</Link>
-                <Link className={styles.title} href="/semester/1">Primeiro</Link>
-                <Link className={styles.title} href="/semester/2">Segundo</Link>
-                <Link className={styles.title} href="/semester/3">Terceiro</Link>
-                <Link className={styles.title} href="/semester/4">Quarto</Link>
-                <Link className={styles.title} href="/semester/5">Quinto</Link>
-                <Link className={styles.title} href="/semester/6">Sexto</Link>
-                <Link className={styles.title} href="/semester/7">Sétimo</Link>
+                <Link className={styles.navbarText} href="/">Home</Link>
+                <Link className={styles.navbarText} href="/semester/1">Primeiro</Link>
+                <Link className={styles.navbarText} href="/semester/2">Segundo</Link>
+                <Link className={styles.navbarText} href="/semester/3">Terceiro</Link>
+                <Link className={styles.navbarText} href="/semester/4">Quarto</Link>
+                <Link className={styles.navbarText} href="/semester/5">Quinto</Link>
+                <Link className={styles.navbarText} href="/semester/6">Sexto</Link>
+                <Link className={styles.navbarText} href="/semester/7">Sétimo</Link>
             </div>
           
         </nav>
